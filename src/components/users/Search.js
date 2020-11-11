874b9d0b0f3308baabf83204f2464d4af86e50b4
andre <ahochschulte.apple@gmail.com>
2020-11-11 21:58:15 +0100

    Initial Commit

---
+++ src/components/users/Search.js
@@ -0,0 +1,53 @@
+import { useState, useContext } from 'react';
+import GithubContext from '../../context/github/githubContext';
+import AlertContext from '../../context/alert/alertContext';
+
+const Search = () => {
+  const githubContext = useContext(GithubContext);
+  const alertContext = useContext(AlertContext);
+
+  const { searchUsers, clearUsers, users } = githubContext;
+  const { showAlert } = alertContext;
+
+  const [text, setText] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (text === '') {
+      showAlert('Please enter something', 'light');
+    } else {
+      searchUsers(text);
+      setText('');
+    }
+  };
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <div>
+      <form className='form' onSubmit={onSubmit}>
+        <input
+          type='text'
+          name='text'
+          placeholder='Search Users...'
+          value={text}
+          onChange={onChange}
+        />
+        <input
+          type='submit'
+          value='Search'
+          className='btn btn-dark btn-block'
+        />
+      </form>
+      {users.length > 0 && (
+        <button className='btn btn-light btn-block' onClick={clearUsers}>
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Search;
---
+++ src/context/alert/AlertState.js
@@ -0,0 +1,30 @@
+import React, { useReducer } from 'react';
+import AlertReducer from './alertReducer';
+import AlertContext from './alertContext';
+import { SET_ALERT, REMOVE_ALERT } from '../types';
+
+const AlertState = (props) => {
+  const initialState = null;
+
+  const [state, dispatch] = useReducer(AlertReducer, initialState);
+
+  const showAlert = (msg, type) => {
+    dispatch({ type: SET_ALERT, payload: { msg: msg, type: type } });
+    setTimeout(() => {
+      dispatch({ type: REMOVE_ALERT });
+    }, 5000);
+  };
+
+  return (
+    <AlertContext.Provider
+      value={{
+        alert: state,
+        showAlert,
+      }}
+    >
+      {props.children}
+    </AlertContext.Provider>
+  );
+};
+
+export default AlertState;
